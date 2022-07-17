6363e9e1705bfd451a387e8110a8938695239834
denist2322 <parksomang125@gmail.com>
2022-07-17 15:02:02 +0900

    User 엔티티 오류 수정 및 회원가입

--- src/main/java/com/mysite/sbb33/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb33/controller/ArticleController.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
-@RequestMapping("/usr/article")
+@RequestMapping("/article")
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
--- src/main/java/com/mysite/sbb33/vo/User.java
+++ src/main/java/com/mysite/sbb33/vo/User.java
@@ -18,7 +18,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime regDate;
-    private LocalDateTime UpDate;
+    private LocalDateTime UpdateDate;
     private String email;
     private String name;
     @JsonIgnore
